9cb44b74cae34bc989a90a0007f160814b8aa4b6
Abhi <Abhi@Abhi-PC>
2018-01-06 15:01:14 +0530

    Singleton Design Pattern Eager Initialization approach

---
+++ src/com/codepumpkin/creational/singleton/EagerInitializedSingleton.java
@@ -0,0 +1,17 @@
+package com.codepumpkin.creational.singleton;
+
+public class EagerInitializedSingleton {
+	 
+    // Private static instance is created at the time of class loading
+    private static EagerInitializedSingleton instance = new EagerInitializedSingleton();
+ 
+    // Constructor made private so that no new instance can be created
+    private EagerInitializedSingleton() {
+ 
+    }
+ 
+    // Static method to return the instance to the client
+    public static EagerInitializedSingleton getInstance() {
+        return instance;
+    }
+}
